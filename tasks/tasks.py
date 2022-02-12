c13a8db58bdf6631910908aeee37301a4ed00b7b
Ashesh3 <3626859+Ashesh3@users.noreply.github.com>
2022-02-12 07:36:40 +0000

    change last sent only if send_mail is successful Merge matching new and missing users into the same method
    Signed-off-by: Ashesh3 <3626859+Ashesh3@users.noreply.github.com>

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -1,9 +1,11 @@
-from tasks.models import STATUS_CHOICES, Task, UserPreferences
-from task_manager.celery import app
-from django.core.mail import send_mail
 from datetime import datetime, timedelta
+from smtplib import SMTPException
 
 from celery.decorators import periodic_task
+from django.core.mail import send_mail
+from task_manager.celery import app
+
+from tasks.models import STATUS_CHOICES, Task, UserPreferences
 
 
 @app.task
@@ -17,27 +19,19 @@ def process_email(user):
             email_content += f"\n{len(tasks)} {status[1].lower()} task(s).\n"
     email_content += "\n\n"
     email_content += "Thank you!"
-    send_mail("Tasks summary", email_content, "tasks@task_manager.org", [user.email], fail_silently=False,)
-
-
-def process_missing_users():
-    users = UserPreferences.objects.filter(reminder_enabled=True, last_sent__lte=datetime.now() - timedelta(hours=24))
-    for user in users:
-        process_email.delay(user.user)
+    try:
+        send_mail("Tasks summary", email_content, "tasks@task_manager.org", [user.email], fail_silently=False,)
         user.last_sent = datetime.now()
         user.save()
+    except SMTPException as e:
+        print("Error sending email", e)
 
 
 @periodic_task(run_every=timedelta(seconds=30))
 def send_reports():
-    current_time = datetime.now()
-    matching_users = UserPreferences.objects.filter(reminder_enabled=True, reminder_time__range=(current_time - timedelta(seconds=30), current_time))
-    if len(matching_users) == 0:
+    new_and_missing_users = UserPreferences.objects.filter(reminder_enabled=True, last_sent__lte=datetime.now() - timedelta(hours=24))
+    if len(new_and_missing_users) == 0:
         print("No matching users found")
-    for user_prefs in matching_users:
+    for user_prefs in new_and_missing_users:
         print("Processing", user_prefs.user)
         process_email.delay(user_prefs.user)
-        user_prefs.last_sent = datetime.now()
-        user_prefs.save()
-
-    process_missing_users()
